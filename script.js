88c54db55448187d155035a961d856d7e3d372c1
Khang Duong Van <dvkhang2100321@student.ctuet.edu.vn>
2023-10-11 18:47:11 +0700

    upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let selectField = document.getElementById("selectField");
+let selectText = document.getElementById("selectText");
+let options = document.getElementsByClassName("options");
+let list = document.getElementById("list");
+let arrowIcon = document.getElementById("arrowIcon");
+selectField.onclick = function () {
+  list.classList.toggle("hide");
+  arrowIcon.classList.toggle("rotate");
+};
+for (option of options) {
+  option.onclick = function () {
+    selectText.innerHTML = this.textContent;
+    list.classList.toggle("hide");
+    arrowIcon.classList.toggle("rotate");
+  };
+}
+// [...options].forEach((option) => {
+//   option.onclick = function () {
+//     selectText.innerHTML = option.textContent;
+//     console.log(this);
+//   };
+// });
